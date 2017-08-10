842ca855183471c9080275e5c29efe3799c29c27
Cody Chase <cchase1991@gmail.com>
2017-08-10 11:59:10 -0400

    broken

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  user: "root",
+  password: "motiv8er",
+  database: "burgers_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
